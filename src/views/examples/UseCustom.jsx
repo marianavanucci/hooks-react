828185e264b1ed188844ffc8ebc06ee3504eddbe
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-21 20:41:10 -0300

    create custom hook useCounter( )

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import '../../components/layout/SectionTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useCounter } from '../../hooks/useCounter'
 
 const UseRef = (props) => {
+
+    const [ count, inc, dec] = useCounter ()
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+        <SectionTitle title='Exercício 01'></SectionTitle>
+        <div className="center">
+
+            <span className="text">{count}</span>
+
+            <div>
+                <button className="btn"
+                onClick={ () => inc ()}>Inc</button>
+                <button className="btn"
+                onClick={ () => dec ()}>Dec</button>
+            </div>
+
+        </div>
+            
         </div>
     )
 }
--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,38 +1,38 @@
-import React, { useReducer } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+// import React, { useReducer } from 'react'
+// import PageTitle from '../../components/layout/PageTitle'
 
-import { inicialState, reducer } from '../../store/'
-import {numberAdd2} from '../../store/actions/number'
-
-
+// import { inicialState, reducer } from '../../store/'
+// import {numberAdd2} from '../../store/actions/number'
 
 
-const UseReducer = (props) => {
 
-    const [state, dispatch] = useReducer(reducer, inicialState)
 
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-            <div className="center">
-                {state.user ? 
-                    <span className="text">{state.user.name }</span> 
-                    : <span className="text">Sem usuário</span>
-                }
+// const UseReducer = (props) => {
+
+//     const [state, dispatch] = useReducer(reducer, inicialState)
+
+//     return (
+//         <div className="UseReducer">
+//             <PageTitle
+//                 title="Hook UseReducer"
+//                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
+//             />
+//             <div className="center">
+//                 {state.user ? 
+//                     <span className="text">{state.user.name }</span> 
+//                     : <span className="text">Sem usuário</span>
+//                 }
                 
-                <span className="text">{state.number}</span>
-                <div>
-                    <button className="btn"
-                    onClick={ ()=> dispatch({type: 'login', payload: 'Maria'})}>Login</button>
-                    <button className="btn"
-                    onClick={ ()=> numberAdd2(dispatch)}>+2</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+//                 <span className="text">{state.number}</span>
+//                 <div>
+//                     <button className="btn"
+//                     onClick={ ()=> dispatch({type: 'login', payload: 'Maria'})}>Login</button>
+//                     <button className="btn"
+//                     onClick={ ()=> numberAdd2(dispatch)}>+2</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default UseReducer
+// export default UseReducer
