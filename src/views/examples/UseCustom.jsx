2e1afd0f698babd211593e277c423fee5207410d
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-21 21:09:49 -0300

    useFecth custom hook

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -3,10 +3,22 @@ import PageTitle from '../../components/layout/PageTitle'
 import '../../components/layout/SectionTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
 
 const UseRef = (props) => {
 
-    const [ count, inc, dec] = useCounter ()
+    const [ count, inc, dec] = useCounter (10)
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+
+    const response = useFetch(url)
+
+    function showStates(states){
+        return states.map( state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
+
+    // fetch('http://files.cod3r.com.br/curso-react/estados.json')
+    // .then(resp => resp.json())
+    // .then(json => console.log(json))
 
     return (
         <div className="UseCustom">
@@ -27,7 +39,12 @@ const UseRef = (props) => {
             </div>
 
         </div>
-            
+
+        <SectionTitle title='Exercício 02'></SectionTitle>
+        <div className="center"></div>
+            <ul>
+                {response.data ? showStates(response.data) : false }
+            </ul>
         </div>
     )
 }
